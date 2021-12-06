94331048fff62a4a7c50d9fe550248f349081205
LeoAChaves <leochaves2121@gmail.com>
2021-12-06 15:10:40 -0300

    Crifa de Cesar

--- javaScript/base64.js
+++ javaScript/base64.js
@@ -1,11 +1,11 @@
 var resposta = document.querySelector(".resposta");
 
 function codificarBase64(mensagem) {
-  const codigo = btoa(mensagem);
+  var codigo = btoa(mensagem);
   resposta.innerText = codigo;
 }
 
 function decodificarBase64(codigo) {
-  const mensagem = atob(codigo);
+  var mensagem = atob(codigo);
   resposta.innerText = mensagem;
 }
--- javaScript/cifraDeCesar.js
+++ javaScript/cifraDeCesar.js
@@ -1,3 +1,34 @@
 var resposta = document.querySelector(".resposta");
 
-function codificarCifra(mensagem, chave) {}
+function codificarCifra(mensagem, chave) {
+  mensagem = mensagem.split("");
+  chave = parseInt(chave, 10);
+  var codigo = "";
+  for (var i = 0; i < mensagem.length; i++) {
+    var charCode = mensagem[i].charCodeAt();
+    if (charCode >= 65 && charCode <= 90) {
+      codigo += String.fromCharCode(((charCode + chave - 65) % 26) + 65);
+    } else if (charCode >= 97 && charCode <= 122) {
+      codigo += String.fromCharCode(((charCode + chave - 97) % 26) + 97);
+    } else {
+      codigo += mensagem[i];
+    }
+  }
+  resposta.innerText = codigo;
+}
+function decodificarCifra(mensagem, chave) {
+  mensagem = mensagem.split("");
+  chave = parseInt(chave, 10);
+  var codigo = "";
+  for (var i = 0; i < mensagem.length; i++) {
+    var charCode = mensagem[i].charCodeAt();
+    if (charCode >= 65 && charCode <= 90) {
+      codigo += String.fromCharCode(((charCode - chave - 90) % 26) + 90);
+    } else if (charCode >= 97 && charCode <= 122) {
+      codigo += String.fromCharCode(((charCode - chave - 122) % 26) + 122);
+    } else {
+      codigo += mensagem[i];
+    }
+  }
+  resposta.innerText = codigo;
+}
